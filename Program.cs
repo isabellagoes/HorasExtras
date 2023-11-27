b6629ac36bf2ffdc0d6b855a9d914a724eb8dee5
isabellagoes <isabellagoes.igs@gmail.com>
2023-11-26 22:25:00 -0300

    Exercício resolvido depois de ter interpretado errado a demanda e ter perdido tempo :)

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("------------ Horas Extras ------------\n");
+
+Console.WriteLine($"Salario-hora de {12:C}, {300} horas, sendo {0,2} horas-extra, salário = {Salario(12, 300, 0):C}");
+Console.WriteLine($"Salario-hora de {12:C}, {300} horas, sendo {20} horas-extra, salário = {Salario(12, 300, 20):C}");
+Console.WriteLine($"Salario-hora de {24:C}, {300} horas, sendo {0,2} horas-extra, salário = {Salario(24, 300, 0):C}");
+Console.WriteLine($"Salario-hora de {24:C}, {300} horas, sendo {20} horas-extra, salário = {Salario(24, 300, 20):C}");
+
+double Salario(double SalarioHora, double HorasTotais, double HorasExtras)
+{
+    return (SalarioHora * (HorasTotais - HorasExtras)) + (SalarioHora * HorasExtras * 1.4);
+}
